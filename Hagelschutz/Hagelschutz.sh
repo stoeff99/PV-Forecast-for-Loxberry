1f4899379076f843825f69832867ac701c22c8e9
Chris B <stoeff99@bowles.ch>
2019-04-23 20:53:34 +0200

    Corrected loop for changing variable value

--- Hagelschutz/Hagelschutz.sh
+++ Hagelschutz/Hagelschutz.sh
@@ -3,19 +3,20 @@
 while [ 0 -le 1 ]
 do
 
+curstate2=$curstate
+
 url=https://meteo.netitservices.com/api/v0/devices/MAC_ADDRESS/poll?hwtypeId=ID
 
 curstate=$(curl -sb -H "Accept: application/json" ${url} | cut -c 2-17)
 echo $curstate
 
-if [ "$curstate" != '"currentState":0' ];then
-echo -n "value=$curstate" >/dev/udp/192.168.20.2/7009
-echo "Pull up the blinds! There is a hail storm approaching."
+if [ "$curstate2" != "$curstate" ];then
+echo -n "value=$curstate" >/dev/udp/192.168.20.2/7008
+echo "The Hailstorm Value has Changed."
 else
-echo "There seems to be no Hail Storm"
+echo "The Hailstorm Value has not Changed."
 fi
 
 sleep 120
 
 done
-
